7d3df7d5459ced1e5302e8be4103bba810e2a777
Amit Raturi <scout@soroco.com>
2025-05-22 17:58:30 +0530

    fix(server): use fmt errors instead of creating new

--- server/server.go
+++ server/server.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"errors"
 	"fmt"
 	v1 "githubsearchservice/gen/github.com/akraturi/githubsearchservice/pkg/pb/v1"
 	"githubsearchservice/service/githubservice"
@@ -24,7 +23,7 @@ func New(githubService githubservice.GithubService) Server {
 func (s *Server) Run() error {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
-		return errors.New(fmt.Sprintf("failed to listen: %v", err))
+		return fmt.Errorf("failed to listen: %v", err)
 	}
 
 	server := grpc.NewServer()
@@ -33,7 +32,7 @@ func (s *Server) Run() error {
 	log.Println("server listening on :50051")
 
 	if err := server.Serve(lis); err != nil {
-		return errors.New(fmt.Sprintf("failed to serve: %v", err))
+		return fmt.Errorf("failed to serve: %v", err)
 	}
 
 	return nil
